bc256ecbf7abe8e4e1f043edad9f4e881da8cf4f
Alejo9601 <alejandro96jm@gmail.com>
2022-09-04 23:25:41 -0300

    Deleted useless code

--- src/routes/v1/loginRoutes.js
+++ src/routes/v1/loginRoutes.js
@@ -4,7 +4,6 @@ const loginRoutes = express.Router();
 
 loginRoutes.post("/", (req, res, next) => {
   const user = req.body;
-  console.log(user);
   validateUser(user.username, user.password).then((result) =>
     res.send(result).end()
   );
